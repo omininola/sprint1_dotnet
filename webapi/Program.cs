803783b17b94204a4c7c70b448edf8a793b0a8cd
Otavio Miklos <otaviomiklos@gmail.com>
2025-05-22 13:09:57 -0300

    chore: changing swagger to always be visible

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -17,11 +17,8 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseOracle(builder
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
--- webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net8.0/webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e288b446e14d1c64da6b4345371bcff050bdda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dd0866280763ecccba7b0688c2d59586c4ba6ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
